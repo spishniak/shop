7ea1237f32cbd82ea7d87566398b708203b9071a
ivan <spishniak@gmail.com>
2021-04-24 12:51:01 +0300

    fersion 1.0

--- shop.js
+++ shop.js
@@ -1,1 +1,18 @@
-require('console-input')
+const input = require('console-input').input
+
+console.log('Hello');
+console.log('What do you want');
+const answer = input();
+
+if (answer === 'vodka') {
+    console.log('how old are you?')
+    const age = input()
+    
+    if (age >= 18) {
+        console.log('Ok')
+    } else {
+        console.log('NO');
+    }
+} else {
+    console.log('Yes')
+}
